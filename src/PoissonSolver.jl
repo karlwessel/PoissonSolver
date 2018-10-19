721db957132da9beb68cb15439cde3cfd47a152f
karl.wessel <karl.wessel@stud.uni-goettingen.de>
2018-10-19 16:47:31 +0200

    added some documentation

--- src/PoissonSolver.jl
+++ src/PoissonSolver.jl
@@ -22,10 +22,26 @@ function readvolume(file::String, N::Int, M::Int, L::Int)
 end
 readvolume(file::String, N::Int) = readvolume(file, N, N, N)
 
+"""
+Write a N x M x L - array (volume) to the file with the passed
+path.
+
+Data is stored in C-compatible float32 binary format in
+row-major-order.
+"""
 function writevolume(file::String, v::AbstractArray)
     write(open(file, "w"), permutedims(v, (3,2,1)))
 end
 
+"""
+Execute the passed amount of time steps for the passed initial condition
+and sample distance.
+
+v - initial condition (IC) of the 3d diffusion equation to solve
+dx, dy, dz - the sampling distance of the IC
+step - the number of time steps to forward the IC
+dt - the size of one time step
+"""
 function diffusion(v::AbstractArray{T, 3}, dx, dy, dz, steps, dt) where {T}
     println("Creating Plan for $(N)...")
     plan = DiffusionPlan(size(v), dx, dy, dz, dt)
@@ -35,9 +51,35 @@ function diffusion(v::AbstractArray{T, 3}, dx, dy, dz, steps, dt) where {T}
     execute!(plan, vgpu, steps)
     collect(vgpu)
 end
+"""
+Execute the passed amount of time steps for the passed initial condition
+and sample distance.
+
+Assumes the domain of the input data is zero to one and therefore the
+sampling distance is 1 / size(v, i) in dimension i.
+
+v - initial condition (IC) of the 3d diffusion equation to solve
+step - the number of time steps to forward the IC
+dt - the size of one time step
+"""
 diffusion(v::AbstractArray{T, 3}, steps, dt) where {T} =
         diffusion(v, 1 ./ size(v), steps, dt)
 
+"""
+Execute the passed amount of time steps for the passed initial condition
+and sample distance.
+
+Assumes the domain of the input data is zero to one and therefore the
+sampling distance is 1 / size(v, i) in dimension i.
+
+ic - path to the file containing the initial condition (IC) of the 3d diffusion
+    equation to solve. The data should be stored in c compatible binary format
+    of float32 values.
+out - path to the file where the result should be stored to. Data format of the
+    result will be the same as for the input IC file.
+step - the number of time steps to forward the IC
+dt - the size of one time step
+"""
 function diffusion(ic::String, out::String, N::Int, steps::Int, dt::Real)
     println("Reading input from $(ic)...")
     v = readvolume(ic, N)
